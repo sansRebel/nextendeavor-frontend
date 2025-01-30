ed580fc0e1abcca75424181e42a5a10ce32fd24f
sansRebel <khaledalsinafi101@gmail.com>
2025-01-31 00:49:30 +0800

    Added token setting to authentication services

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,12 +1,15 @@
 import apiClient from "./apiClient";
 import { AuthResponse } from "@/types/index";
+import { setToken } from "@/utils/token";
 
 export const signup = async (name: string, email: string, password: string) => {
   const res = await apiClient.post<AuthResponse>("/auth/signup", { name, email, password });
+  setToken(res.data.token);
   return res.data;
 };
 
 export const login = async (email: string, password: string) => {
   const res = await apiClient.post<AuthResponse>("/auth/login", { email, password });
+  setToken(res.data.token);
   return res.data;
 };
